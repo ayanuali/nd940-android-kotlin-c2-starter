04abdff1adea45d19b18e00545eab8f9f707a705
Ayan Uali <ayanuali@gmail.com>
2021-10-03 19:41:41 +0600

    fixed issue with service call

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,8 +1,10 @@
 package com.udacity.asteroidradar
 
 object Constants {
-    const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
+    const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
+    //to use with start and end dates (2 days)
+    const val DEFAULT_END_DATE_TWO_DAYS = 1
     const val BASE_URL = "https://api.nasa.gov/neo/rest/v1/"
     const val API_KEY = "gSM0fR1tNTBssH7tUhu9kwCXmszH6h0T3Zcd0Bey"
 }
--- starter/app/src/main/java/com/udacity/asteroidradar/network/Service.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/network/Service.kt
@@ -8,24 +8,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import kotlinx.coroutines.Deferred
+import okhttp3.ResponseBody
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Query
 
 interface Service {
-    @GET("feed?api_key=gSM0fR1tNTBssH7tUhu9kwCXmszH6h0T3Zcd0Bey")
+    //  not providing start and end dats pulls data for the last 8 days including today
+//        @Query("start_date") start_date: String,
+//        @Query("end_date") end_date: String,
+    @GET("feed")
     fun getAsteroids(
         @Query("start_date") start_date: String,
         @Query("end_date") end_date: String,
         @Query("api_key") api_key: String
-    ): Deferred<Asteroids>
+    ): Call<String>
 }
-
-/**
- * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
- * full Kotlin compatibility.
- */
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
 
 /**
  * Main entry point for network access. Call like `Network.devbytes.getPlaylist()`
@@ -33,7 +32,7 @@ private val moshi = Moshi.Builder()
 
 private val retrofit = Retrofit.Builder()
     .baseUrl(Constants.BASE_URL)
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addConverterFactory(ScalarsConverterFactory.create())
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .build()
 
